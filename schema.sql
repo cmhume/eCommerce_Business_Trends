5d27fbb4f9d89208d52554d2e8282f2494f9ab09
Luz Maria <luzmariacabral80@gmail.com>
2021-07-28 11:05:49 -0700

    Schema for csv files

--- schema.sql
+++ schema.sql
@@ -1,31 +1,33 @@
 -- Creating tables for eCommerce_dataDB
-CREATE TABLE customers (
+CREATE TABLE olist_geolocation_dataset (
+	geolocation_zip_code_prefix INT NOT NULL,
+	geolocation_lat INT NOT NULL,
+	geolocation_lng INT NOT NULL,
+	geolocation_city VARCHAR NOT NULL,
+	geolocation_state VARCHAR NOT NULL,
+	PRIMARY KEY (geolocation_zip_code_prefix, geolocation_city, geolocation_state)
+);
+CREATE TABLE olist_customers_dataset (
 	customer_id VARCHAR NOT NULL,
+	customer_unique_id VARCHAR NOT NULL,
 	customer_zip_code_prefix INT NOT NULL,
 	customer_city VARCHAR NOT NULL,
-	customer_state VARCHAR NOT NULL,
-FOREIGN KEY (customer_zip_code_prefix) REFERENCES Geological_Dataset (geological_zip_code_prefix),
-FOREIGN KEY (customer_city) REFERENCES Geological_Dataset (geological_city),
-FOREIGN KEY (customer_state) REFERENCES Geological_Dataset (geological_state),
-	PRIMARY KEY (customer_id),
-	
+	customer_state VARCHAR NOT NULL
+FOREIGN KEY (customer_zip_code_prefix) REFERENCES olist_geolocation_dataset (geolocation_zip_code_prefix),
+FOREIGN KEY (customer_city) REFERENCES olist_geolocation_dataset (geolocation_city),
+FOREIGN KEY (customer_state) REFERENCES olist_geolocation_dataset (geolocation_state),
+	PRIMARY KEY (customer_id)
 );
-CREATE TABLE order_items (
-	order_id VARCHAR NOT NULL,
-	order_item_id INT NOT NULL,
-	product_id VARCHAR NOT NULL,
-	seller_id VARCHAR NOT NULL,
-	shipping_limit_date DATE NOT NULL,
-	price INT NOT NULL,
-	freight_value INT NOT NULL,
-FOREIGN KEY (product_id) REFERENCES Products_Dataset (product_id),
-	PRIMARY KEY (order_id, product_id)
+CREATE TABLE product_category_name_translation (
+	product_category_name VARCHAR NOT NULL,
+	product_category_name_english VARCHAR NOT NULL,
+	PRIMARY KEY (product_category_name)
 );
-CREATE TABLE Products_Dataset (
+CREATE TABLE olist_products_dataset (
 	product_id VARCHAR NOT NULL,
 	product_category_name VARCHAR NOT NULL,
 	product_name_length INT NOT NULL,
-	product_description_length INT NOT NULL,
+	product_description_lenght INT NOT NULL,
 	product_photos_qty INT NOT NULL,
 	product_weight_g INT NOT NULL,
 	product_length_cm INT NOT NULL,
@@ -34,30 +36,28 @@ CREATE TABLE Products_Dataset (
 FOREIGN KEY (product_category_name) REFERENCES product_category_name_translation (product_category_name),
 	PRIMARY KEY (product_id)
 );
-CREATE TABLE product_category_name_translation (
-	product_category_name VARCHAR NOT NULL,
-	product_category_name_english VARCHAR NOT NULL,
-	PRIMARY KEY (product_category_name)
+CREATE TABLE olist_order_items_dataset (
+	order_id VARCHAR NOT NULL,
+	order_item_id INT NOT NULL,
+	product_id VARCHAR NOT NULL,
+	seller_id VARCHAR NOT NULL,
+	shipping_limit_date DATE NOT NULL,
+	price INT NOT NULL,
+	freight_value INT NOT NULL,
+FOREIGN KEY (product_id) REFERENCES olist_products_dataset (product_id),
+	PRIMARY KEY (order_id)
 );
-CREATE TABLE Geological_Dataset (
-	geological_zip_code_prefix INT NOT NULL,
-	geological_lat INT NOT NULL,
-	geological_lng INT NOT NULL,
-	geological_city VARCHAR NOT NULL,
-	geological_state VARCHAR NOT NULL,
-	PRIMARY KEY (geological_zip_code_prefix, geological_city, geological_state)
-);
-CREATE TABLE Orders_dataset (
+
+CREATE TABLE olist_orders_dataset (
 	order_id VARCHAR NOT NULL,
 	customer_id VARCHAR NOT NULL,
 	order_status VARCHAR NOT NULL,
 	order_purchase_timestamp DATE NOT NULL,
-	order_approved_date DATE NOT NULL,
-	order_delivered__carrier_date DATE NOT NULL,
+	order_approved_at DATE NOT NULL,
+	order_delivered_carrier_date DATE NOT NULL,
 	order_delivered_customer_date DATE NOT NULL,
 	order_estimated_delivery_date DATE NOT NULL,
-FOREIGN KEY (customer_id) REFERENCES Customers_dataset (customer_id),
 	PRIMARY KEY (order_id)
 );
 
-SELECT * FROM customers
+SELECT * FROM product_category_name_translation;
