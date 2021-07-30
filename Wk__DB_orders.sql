31305c1985f295618599131190d3154b92eb57e2
Luz Maria <luzmariacabral80@gmail.com>
2021-07-30 15:24:03 -0700

    changes to dataset

--- Wk__DB_orders.sql
+++ Wk__DB_orders.sql
@@ -66,7 +66,9 @@ SELECT olist_customers_dataset.customer_id,
 	product_category_name_translation.product_category_name_english,
 	olist_customers_dataset.customer_zip_code_prefix,
 	olist_customers_dataset.customer_city,
-	olist_customers_dataset.customer_state
+	olist_customers_dataset.customer_state,
+	order_item_revised.price,
+	order_item_revised.freight_value
 INTO product_data
 FROM product_revised
 INNER JOIN product_category_name_translation
@@ -78,6 +80,15 @@ ON order_list_revised.order_id = order_item_revised.order_id
 JOIN olist_customers_dataset
 on olist_customers_dataset.customer_id = order_list_revised.customer_id;
 
+--Changing column names
+ALTER TABLE product_data
+RENAME product_category_name_english TO product_category;
+ALTER TABLE product_data
+RENAME customer_zip_code_prefix TO zip_code;
+ALTER TABLE product_data
+RENAME customer_city TO city;
+ALTER TABLE product_data
+RENAME customer_state TO state;
 
  --random selection
 CREATE TABLE product_sample AS
@@ -87,5 +98,5 @@ CREATE TABLE product_sample AS
 
 SELECT * FROM product_sample;
 
-DROP TABLE olist_products_dataset;
+DROP TABLE product_sample;
 
