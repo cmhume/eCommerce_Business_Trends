4c02387fbd3b276080bf88783dd1c7fa0b8a49f3
Luz Maria <luzmariacabral80@gmail.com>
2021-07-30 13:25:58 -0700

    Updated schema

--- Wk__DB_orders.sql
+++ Wk__DB_orders.sql
@@ -59,8 +59,33 @@ CREATE TABLE order_list_revised (
 	order_estimated_delivery_date VARCHAR NOT NULL,
 	PRIMARY KEY (order_id)
 );
+-- Joining product datase and product category names
+SELECT olist_customers_dataset.customer_id,
+	order_item_revised.order_id,
+	product_revised.product_id,
+	product_category_name_translation.product_category_name_english,
+	olist_customers_dataset.customer_zip_code_prefix,
+	olist_customers_dataset.customer_city,
+	olist_customers_dataset.customer_state
+INTO product_data
+FROM product_revised
+INNER JOIN product_category_name_translation
+ON product_revised.product_category_name = product_category_name_translation.product_category_name
+JOIN order_item_revised
+ON order_item_revised.product_id = product_revised.product_id
+JOIN order_list_revised
+ON order_list_revised.order_id = order_item_revised.order_id
+JOIN olist_customers_dataset
+on olist_customers_dataset.customer_id = order_list_revised.customer_id;
 
-SELECT * FROM order_list_revised;
 
-DROP TABLE order_item_revised;
+ --random selection
+CREATE TABLE product_sample AS
+	SELECT * FROM product_data
+ 	ORDER BY RANDOM()
+ 	LIMIT 150;
+
+SELECT * FROM product_sample;
+
+DROP TABLE olist_products_dataset;
 
