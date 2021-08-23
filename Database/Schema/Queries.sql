1150737522d5f32359051bb6f4ea23a7c87b130e
Corinne Hume <hume.corinne@gmail.com>
2021-08-22 19:38:56 -0700

    Update Queries.sql

--- Database/Schema/Queries.sql
+++ Database/Schema/Queries.sql
@@ -38,3 +38,105 @@ CREATE TABLE product_sample AS
 SELECT * FROM product_sample;
 
 DROP TABLE product_sample;
+
+
+SELECT *  
+  INTO cust_loc
+  FROM customers_data
+INNER JOIN geolocation_data
+  ON geolocation_data.geolocation_zip_code_prefix = customers_data.customer_zip_code_prefix;
+
+ALTER TABLE cust_loc
+RENAME COLUMN customer_id TO cust_id;
+
+SELECT *
+  INTO cust_loc_orders
+  FROM cust_loc
+INNER JOIN orders_data
+  ON orders_data.customer_id = cust_loc.cust_id;
+
+ALTER TABLE cust_loc_orders
+RENAME COLUMN order_id TO order_id1;
+SELECT *
+  INTO cust_loc_orders_items
+  FROM cust_loc_orders
+INNER JOIN order_item_data
+  ON order_item_data.order_id = cust_loc_orders.order_id1;
+
+ALTER TABLE cust_loc_orders_items
+DROP COLUMN order_id;
+SELECT *
+  INTO cust_loc_orders_items_reviews
+  FROM cust_loc_orders_items
+INNER JOIN order_reviews_data
+  ON order_reviews_data.order_id = cust_loc_orders_items.order_id1;
+
+ALTER TABLE cust_loc_orders_items_reviews
+DROP COLUMN order_id;
+
+SELECT *
+  INTO cust_loc_orders_items_reviews_payment
+  FROM cust_loc_orders_items_reviews
+INNER JOIN order_payment_data
+  ON order_payment_data.order_id = cust_loc_orders_items_reviews.order_id1;
+
+ALTER TABLE cust_loc_orders_items_reviews_payment
+rename COLUMN product_id to product_id1;
+
+SELECT *
+  INTO cust_loc_orders_items_reviews_payment_product
+  FROM cust_loc_orders_items_reviews_payment
+INNER JOIN products_data
+  ON products_data.product_id = cust_loc_orders_items_reviews_payment.product_id1;
+
+ALTER TABLE cust_loc_orders_items_reviews_payment_product
+rename COLUMN product_category_name to product_category_name1;
+SELECT *
+  INTO cust_loc_orders_items_reviews_payment_product_category
+  FROM cust_loc_orders_items_reviews_payment_product
+INNER JOIN product_category_data
+  ON product_category_data.product_category_name = cust_loc_orders_items_reviews_payment_product.product_category_name1;
+
+
+ALTER TABLE cust_loc_orders_items_reviews_payment_product_category
+rename COLUMN seller_id to seller_id1;
+SELECT *
+  INTO olist_final_data
+  FROM cust_loc_orders_items_reviews_payment_product_category
+INNER JOIN seller_data
+  ON seller_data.seller_id = cust_loc_orders_items_reviews_payment_product_category.seller_id1;
+
+select * from olist_final_data;
+
+ALTER TABLE olist_final_data
+DROP COLUMN customer_unique_id;
+
+ALTER TABLE olist_final_data
+DROP COLUMN geolocation_zip_code_prefix;
+
+
+ALTER TABLE olist_final_data
+rename COLUMN customer_zip_code_prefix to zipcode;
+
+
+ALTER TABLE olist_final_data
+DROP COLUMN  seller_id;
+
+ALTER TABLE olist_final_data
+DROP COLUMN  order_id1;
+ALTER TABLE olist_final_data
+DROP COLUMN  product_id1;
+
+ALTER TABLE olist_final_data
+DROP COLUMN  cust_id;
+ALTER TABLE olist_final_data
+rename COLUMN  seller_id1 to seller_id;
+ALTER TABLE olist_final_data
+DROP COLUMN  product_category_name1;
+
+ALTER TABLE olist_final_data
+DROP COLUMN  product_category_name;
+
+
+
+select * from olist_final_data
